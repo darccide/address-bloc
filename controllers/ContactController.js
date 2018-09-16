ec66531ceeb7f6eff4ff1145e1c82b6d89114e80
Steven Thomson <steven.thomson88@gmail.com>
2018-09-16 23:51:06 +0900

    Checkpoint Creating Contacts and Connecting to a Database: Error with model/index

---
+++ controllers/ContactController.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+
+
+module.exports = class ContactController {
+
+  constructor(){
+    this.contacts = [];
+  }
+
+  addContact(name, phone){
+    this.contacts.push({name, phone});
+  }
+
+}
